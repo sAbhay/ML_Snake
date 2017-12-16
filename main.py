6bb1228cdff99ba7cdb898b0e6ec4541f493ba5e
Abhay Singhal <abhayaaditsinghal@gmail.com>
2017-12-16 18:04:11 +0530

    Change #2
    - Changed the input data of the snake to include the state of all tiles

--- main.py
+++ main.py
@@ -19,16 +19,25 @@ currentFood = 0
 
 addedTime = 5 * grid[0]
 
+tiles = []
+
+for i in range(0, grid[0]):
+    tiles.append([])
+    for j in range(0, grid[1]):
+        tiles[i].append(0)
+
 for i in range(400):
     food.append([numpy.random.randint(0, grid[0]-1), numpy.random.randint(0, grid[1]-1)])
+
+tiles[food[currentFood][0]][food[currentFood][1]] = 3
 
 currentDir = 0
 
 pygame.init()
 
 flags = DOUBLEBUF
-# screen = pygame.display.set_mode((size[0], size[1]), flags)
-# screen.set_alpha(None)
+screen = pygame.display.set_mode((size[0], size[1]), flags)
+screen.set_alpha(None)
 
 # time.sleep(0.5)
 
@@ -49,8 +58,14 @@ class Snake:
         self.prevLast = self.tiles[len(self.tiles)-1]
 
     def display(self):
-        for i in range(len(self.tiles)):
-            pygame.draw.rect(screen, (255, 255, 255), (self.tiles[i][0] * tileSize[0], self.tiles[i][1] * tileSize[1], tileSize[0], tileSize[1]))
+        for i in range(len(self.tiles)-1):
+            pygame.draw.rect(screen, (255, 255, 255), (self.tiles[i+1][0] * tileSize[0], self.tiles[i+1][1] * tileSize[1], tileSize[0], tileSize[1]))
+        pygame.draw.rect(screen, (255, 0, 0), (self.tiles[0][0] * tileSize[0], self.tiles[0][1] * tileSize[1], tileSize[0], tileSize[1]))
+
+    def updateTiles(self):
+        for i in self.tiles:
+            tiles[int(i[0])][int(i[1])] = 1
+        tiles[int(self.tiles[0][0])][int(self.tiles[0][1])] = 2
 
     def move(self, dir):
         self.prevLast = self.tiles[len(self.tiles) - 1]
@@ -70,7 +85,7 @@ class Snake:
         self.tiles.__delitem__(len(self.tiles)-1)
 
     def eat(self):
-        if self.tiles[0] == food[currentFood]:
+        if self.tiles[0][0] == food[currentFood][0] and self.tiles[0][1] == food[currentFood][1]:
             self.tiles.append([self.prevLast[0], self.prevLast[1]])
             return True
         return False
@@ -79,9 +94,7 @@ class Snake:
         if self.tiles.__len__() > 1:
             for i in range(1, len(self.tiles)):
                 if self.tiles[i] == self.tiles[0]:
-                    self.clearSnake()
                     return True
-                    print("Dead")
         return False
 
     def clearSnake(self):
@@ -115,12 +128,12 @@ class Snake:
 
 snake = Snake(grid[0]/2, grid[1]/2)
 
-net = Neural_Network([6, 4])
+net = Neural_Network([3*grid[0]*grid[1], 4])
 
 running = True
 while running:
 
-    # screen.fill((0, 0, 0))
+    screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -144,17 +157,31 @@ while running:
                 if currentDir != 2:
                     currentDir = 3
 
-    netInput = snake.checkObstacles()
+    # netInput = snake.checkObstacles()
+    #
+    # angle = numpy.arctan2((snake.tiles[0][1]*tileSize[1]) - (food[currentFood][1]*tileSize[1]), (snake.tiles[0][0]*tileSize[0]) - (food[currentFood][0]*tileSize[0]))
+    # netInput.append(angle)
+    #
+    # dist = distance(snake.tiles[0], food[currentFood])
+    # netInput.append(dist)
 
-    angle = numpy.arctan2((snake.tiles[0][1]*tileSize[1]) - (food[currentFood][1]*tileSize[1]), (snake.tiles[0][0]*tileSize[0]) - (food[currentFood][0]*tileSize[0]))
-    netInput.append(angle)
+    snake.updateTiles()
 
-    dist = distance(snake.tiles[0], food[currentFood])
-    netInput.append(dist)
+    netInput = []
+
+    for i in range(0, len(tiles)):
+        for j in range(0, len(tiles[i])):
+            temp = tiles[int((i + snake.tiles[0][0]) % grid[0])][int((j + snake.tiles[0][1]) % grid[1])]
+            if temp >= 2:
+                temp -= 1
+            for k in range(0, 3):
+                if temp == k:
+                    netInput.append(1)
+                else:
+                    netInput.append(0)
 
     currentDir = net.getAnswer(netInput)
 
-    # snake.display()
     snake.move(currentDir)
 
     timer -= 1
@@ -165,22 +192,27 @@ while running:
         currentFood += 1
         length += 1
 
-
         notDone = True
         while notDone:
             if any(food[currentFood] == t for t in snake.tiles):
-                food[currentFood] = [numpy.random.randint(0, grid[0]-1), numpy.random.randint(0, grid[1]-1)]
+                # food[currentFood] = [numpy.random.randint(0, grid[0]-1), numpy.random.randint(0, grid[1]-1)]
+                currentFood +=1
                 notDone = True
             else:
                 notDone = False
 
+        tiles[food[currentFood][0]][food[currentFood][1]] = 3
+
         score += 1000
         timer += addedTime
+
+    snake.display()
 
     if snake.die() or timer <= 0:
         if snake.die():
             score -= 200
             died = True
+            snake.clearSnake()
         else:
             died = False
 
@@ -191,10 +223,10 @@ while running:
             highScore = score
             net.bestWB()
             variantNum = 0
-            text = open("wb.txt", 'w')
-            text.write(str(highScore) + "|")
-            text.write(str(net.wb))
-            text.close()
+            with open("wb.txt", 'w') as text:
+                # text.write(str(highScore))
+                text.write(str(net.wb))
+                text.close()
 
             gen += 1
             iteration = 0
@@ -209,12 +241,13 @@ while running:
         iteration += 1
         currentFood = 0
         length = 1
+        tiles[food[currentFood][0]][food[currentFood][1]] = 3
 
     if(len(snake.tiles) != length):
         running = False
 
-    # pygame.draw.rect(screen, (0, 255, 0), (food[currentFood][0] * tileSize[0], food[currentFood][1]*tileSize[1], tileSize[0], tileSize[1]))
+    pygame.draw.rect(screen, (0, 255, 0), (food[currentFood][0] * tileSize[0], food[currentFood][1]*tileSize[1], tileSize[0], tileSize[1]))
 
-    # pygame.display.update()
+    pygame.display.update()
 
 pygame.quit()
